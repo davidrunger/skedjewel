377440879d3b99cb6170752afa2f107fd8bc3d64
David Runger <davidjrunger@gmail.com>
2024-02-06 07:41:29 -0600

    Sleep 1 millisecond before logging exit message
    I am seeing repeated messages in my dokku deployment when exiting, and I
    believe that this will help me to determine why.

--- src/runner.cr
+++ src/runner.cr
@@ -22,9 +22,10 @@ class Skedjewel::Runner
       signal.trap do
         signal_name = signal.to_s.gsub("Signal::", "")
         ::Log.info do
+          sleep(0.001)
           "Thanks for using Skedjewel! " \
-            "Received #{signal_name} signal. " \
-            "Exiting now. (PID:#{Process.pid} time:#{Time.local.to_unix_f})"
+          "Received #{signal_name} signal. " \
+          "Exiting now. (PID:#{Process.pid} time:#{Time.local.to_unix_f})"
         end
         exit(0)
       end
